cac74d4a31cb31e776a1f562da7bb75014a7e190
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-30 18:21:34 +0900

    clip_draw를 draw_girl로 만들어서 애니메이션을 각각 넣을 예정

--- Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -6,26 +6,56 @@ running_girl = load_image('running_girl.png')
 idle_girl = load_image('idle_girl.png')
 
 def handle_events():
-    global running
+    global running, alive
     global x, y
     global dir_x, dir_y
 
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            alive = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+
+                
+            elif event.key == SDLK_ESCAPE:
+                alive = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+    
+    pass
+
+def draw_girl():
     
     pass
 
 
-running = True
+alive = True
 x = 800//2
 frame = 0
 dir =0
 
 
-while running:
+while alive:
     clear_canvas()
     ground.draw(400,300,800,600)
-    running_girl.draw(400,300,200,200)
-    idle_girl.draw(400,500,200,50)
-    #character.clip_draw(~~)
+    #running_girl.draw(400,300,200,200)
+    #idle_girl.draw(400,500,200,50)
+    #draw_girl()
     update_canvas()
     #handle_events()
 
