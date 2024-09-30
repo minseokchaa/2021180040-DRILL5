e48b187f10c8a86258a90b0c20267d5fb67e0947
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-30 17:38:16 +0900

    기초작업 배경 그리기 성공

--- Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -1,2 +1,27 @@
 from pico2d import*
 
+open_canvas()
+ground = load_image('TUK_GROUND.png')
+#character = load_image('미정')
+
+def handle_events():
+    pass
+
+
+running = True
+x = 800//2
+frame = 0
+dir =0
+
+
+while running:
+    clear_canvas()
+    ground.draw(400,300,800,600)
+    #character.clip_draw(~~)
+    update_canvas()
+    #handle_events()
+
+
+
+close_canvas()
+    
