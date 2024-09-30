7102d4733a3165a6e1453067f46496822f3c73f6
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-30 20:52:06 +0900

    draw girl 함수 완성
    키를 눌렀을 때 draw girl 도 key 입력을 받아서 오류가 난 듯 해서, handle event함수에서 어떤 방향으로 움직이고 있는지 저장하는 변수를 이용해서 그림

--- Lecture06_HandlingInputs/move_my_character_with_key.py
+++ Lecture06_HandlingInputs/move_my_character_with_key.py
@@ -8,9 +8,9 @@ running_girl = load_image('running_girl.png')
 idle_girl = load_image('idle_girl.png')
 
 def handle_events():
-    global running, alive
+    global alive
     global x, y
-    global dir_x, dir_y
+    global dir_x, dir_y, key_on
 
     events = get_events()
     for event in events:
@@ -19,63 +19,63 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_x += 1
+                key_on = 1
+
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+                key_on = 2
+
             elif event.key == SDLK_UP:
                 dir_y += 1
+                key_on = 3
+
             elif event.key == SDLK_DOWN:
                 dir_y -= 1
+                key_on = 4
+
             elif event.key == SDLK_ESCAPE:
                 alive = False
 
-                
+
         elif event.type == SDL_KEYUP:
+            
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
+                key_on = 0
+                
             elif event.key == SDLK_LEFT:
                 dir_x += 1
+                key_on = 0
+                
             elif event.key == SDLK_UP:
                 dir_y -= 1
+                key_on = 0
+                
             elif event.key == SDLK_DOWN:
                 dir_y += 1
+                key_on = 0
     
-    pass
+
 
 
 def draw_girl(x,y):
+   
+    if key_on == 0:
+        idle_girl.clip_draw(frame * 320, 0, 320, 320, x, y, 100, 100)
 
-    events = get_events()
+    elif key_on == 4:
+        running_girl.clip_draw(frame * 320, 960, 320, 320, x, y, 100, 100)
 
-    ax = x
-    ay = y
+    elif key_on == 1:
+        running_girl.clip_draw(frame * 320, 640, 320, 320, x, y, 100, 100)
+
+    elif key_on == 2:
+        running_girl.clip_draw(frame * 320, 320, 320, 320, x, y, 100, 100)
+
+    elif key_on == 3:
+        running_girl.clip_draw(frame * 320, 0, 320, 320, x, y, 100, 100)
     
-    for event in events:
-        if event.type == SDL_QUIT:
-            alive = False
-        elif event.type == SDL_KEYDOWN:         #키를 누르면 running애니메이션 출력
-            if event.key == SDLK_RIGHT:
-                running_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_LEFT:
-                running_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_UP:
-                running_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_DOWN:
-                running_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)          
-            elif event.key == SDLK_ESCAPE:
-                alive = False
-                
-        elif event.type == SDL_KEYUP:           #키를 때면 idle애니메이션 출력
-            if event.key == SDLK_RIGHT:
-                idle_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_LEFT:
-                idle_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_UP:
-                idle_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-            elif event.key == SDLK_DOWN:
-                idle_girl.clip_draw(frame*100,100,100,100,ax,ay,150,150)
-    
-    pass
-
+   
 
 alive = True
 x = 800//2
@@ -83,18 +83,22 @@ y = 600//2
 frame = 0
 dir_x = 0
 dir_y = 0
+key_on = 0
 
 while alive:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
+    
     #idle_girl.draw(x,y)
-    #draw_girl(x,y)
+    
+    draw_girl(x,y)
+    
     update_canvas()
     handle_events()
     x += dir_x * 7
     y += dir_y * 7
     frame = (frame + 1)%4
-    delay(0.05)
+    delay(0.1)
 
 
 
